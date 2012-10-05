535b77981f7a4ae1783b0fae7a46751f791b0369
Aditi Mittal <aditi.iitr@gmail.com>
2012-10-05 21:03:59 +0530

    Add app_settings.py module to store app related settings.

---
+++ app/app_settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+File to store the various configurations and settings.
+"""
+
+import os
+
+
+APP_DIRECTORY = os.path.dirname(__file__)
+TEMPLATE_DIR = os.path.join(APP_DIRECTORY, 'templates')
+
+
+
